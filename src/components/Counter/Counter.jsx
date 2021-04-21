9db1d6ff563b338cecb84dad92bcc6a7858bc853
Emre <emrekurtt1655@gmail.com>
2021-04-21 22:43:40 +0200

    finish the app

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Counter.css";
 
 export default function Counter() {
-    return (
-        <div>
-            <h3 data-testid="header">My Counter</h3>
-        </div>
-    )
+  const [counterValue, setCounterValue] = React.useState(0);
+  const [inputValue, setInputValue] = React.useState(1);
+
+  const addToCounter = () => {
+    setCounterValue(counterValue + inputValue);
+  };
+  const subFromCounter = () => {
+    setCounterValue(counterValue - inputValue);
+  };
+
+  return (
+    <div>
+      <h3 data-testid="header">My Counter</h3>
+      <h2 
+      data-testid="counter"
+      className={`${counterValue >= 100 ? "green" : ""}${counterValue <= -100 ? 'red' : ""}`}
+      >
+          {counterValue}
+      </h2>
+      <button data-testid="subBtn" onClick={subFromCounter}>
+        -
+      </button>
+      <input
+        className="text-center"
+        data-testid="input"
+        type="number"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(parseInt(e.target.value));
+        }}
+      />
+      <button data-testid="addBtn" onClick={addToCounter}>
+        +
+      </button>
+    </div>
+  );
 }
--- src/components/Counter/__test__/Counter.test.js
+++ src/components/Counter/__test__/Counter.test.js
@@ -1,11 +1,181 @@
 import React from 'react';
 import Counter from '../Counter';
-import { render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
-test("header renders with correct text", () => {
+let getByTestId;
+
+beforeEach(() => {
     const component = render(<Counter />);
-    const headerEl = component.getByTestId("header");
+    getByTestId = component.getByTestId
+})
+
+afterEach(() => {
+    cleanup();
+})
+
+test("header renders with correct text", () => {
+    const headerEl = getByTestId("header");
 
     expect(headerEl.textContent).toBe("My Counter")
+})
+
+
+test("counter initially start with string of 0", () => {
+    const counterEl = getByTestId("counter");
+
+    expect(counterEl.textContent).toBe("0")
+})
+
+test("counter input initially start with string of 1", () => {
+    const inputEl = getByTestId("input");
+
+    expect(inputEl.value).toBe("1");
+})
+
+test("add button text should contain +", () => {
+    const addBtn = getByTestId("addBtn");
+
+    expect(addBtn.textContent).toBe("+")
+
+})
+
+test("subtract button text should contain - ", () => {
+    const subBtn = getByTestId("subBtn");
+
+    expect(subBtn.textContent).toBe("-")
+
+})
+
+test("change input value properly", () => {
+    const inputEl = getByTestId("input");
+
+    expect(inputEl.value).toBe("1")
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    });
+
+    expect(inputEl.value).toBe("5");
+})
+
+test("click add btn adds input-value to the counter", () => {
+    const addBtnEl = getByTestId("addBtn");
+    const counterEl = getByTestId("counter");
+    const inputEl = getByTestId("input")
+    
+    expect(counterEl.textContent).toBe("0");
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+    fireEvent.click(addBtnEl);
+
+    expect(counterEl.textContent).toBe("5");
+
+})
+
+test("click subtract btn subtracts input-value from the counter", () => {
+    const subBtnEl = getByTestId("subBtn");
+    const counterEl = getByTestId("counter");
+    const inputEl = getByTestId("input")
+    
+    expect(counterEl.textContent).toBe("0");
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+    fireEvent.click(subBtnEl);
+
+    expect(counterEl.textContent).toBe("-5");
+
+})
+
+test("input value changes and multiple add and sub buttons works and gives the correct answer ", () => {
+    const addBtnEl = getByTestId("addBtn");
+    const subBtnEl = getByTestId("subBtn");
+    const counterEl = getByTestId("counter");
+    const inputEl = getByTestId("input")
+
+    expect(counterEl.textContent).toBe("0");
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "20"
+        }
+    })
+    fireEvent.click(addBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(subBtnEl);
+    fireEvent.click(subBtnEl);
+
+    expect(counterEl.textContent).toBe("40");
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "10"
+        }
+    })
+
+    fireEvent.click(addBtnEl);
+    fireEvent.click(subBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(addBtnEl);
+    fireEvent.click(subBtnEl);
+
+    expect(counterEl.textContent).toBe("60");
+
+})
+
+
+test('counterEl should correct classname', () => {
+    const addBtnEl = getByTestId("addBtn");
+    const subBtnEl = getByTestId("subBtn");
+    const counterEl = getByTestId("counter");
+    const inputEl = getByTestId("input")
+
+    expect(counterEl.className).toBe("");
+
+    fireEvent.change(inputEl, {
+        target:{
+            value: "50"
+        }
+    })
+
+    fireEvent.click(addBtnEl)
+    expect(counterEl.className).toBe("");
+
+    fireEvent.click(addBtnEl);
+    expect(counterEl.className).toBe("green");
+
+    fireEvent.click(addBtnEl);
+    expect(counterEl.className).toBe("green");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("green");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("red");
+
+    fireEvent.click(subBtnEl);
+    expect(counterEl.className).toBe("red");
+
+
 })
